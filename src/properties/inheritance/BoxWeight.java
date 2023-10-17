b73d0210911aa7a971541bc79591d97c5d21c7fa
Teo <teodorapop84@yahoo.com>
2023-10-17 14:41:01 +0300

    Inheritance

---
+++ src/properties/inheritance/BoxWeight.java
@@ -0,0 +1,19 @@
+package properties.inheritance;
+
+public class BoxWeight extends Box {
+
+    double weight;
+
+    public BoxWeight(){
+        this.weight = -1;
+    }
+
+    public BoxWeight(double l, double h, double w, double weight) {
+        // call the constructor from Box (parent class)
+        super(l, h, w); // if I don't write this will call the default constructor from parent class !!!
+        this.weight = weight;
+    }
+
+
+
+}
